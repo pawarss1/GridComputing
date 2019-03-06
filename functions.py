823d780dc801f262acd9735f82f29076f5f5a078
ani1311 <aniket.alur13111997@gmail.com>
2019-03-06 22:39:18 +0530

    adding code to distrubte images

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import socket
 import numpy as np
 import cv2
+import io as StringIO
 
 class Client():
     def __init__(self):
@@ -11,16 +12,24 @@ class Client():
         self.s.connect(self.address)
 
     def recieveAndPrintMessage(self):
-        message = self.s.recv(1024).decode()
+        
         print(message)
 
     def closeConnection(self):
         self.s.close()
 
     def getData(self):
-        t = self.s.recv(1024)
-        self.data = np.fromstring(t,dtype=np.float64)
-        print(self.data)
+        gb = bytes(0)
+        while True:
+            rb = self.s.recv(1024)
+            if not rb: 
+                break
+            print("sending %d"%len(gb))
+            gb = gb + rb
+        # t = self.s.recv(1024)
+        self.data = np.fromstring(gb,dtype=np.float64)
+        print(self.data.shape)
+
 
 
 
@@ -40,15 +49,16 @@ class Host():
         self.s.listen(1)
         self.no_of_clients = no_of_clients
         self.clients = []
-        # self.data = np.asarray(cv2.imread('img.jpg'),dtype = "int32")
-        self.data = np.random.rand(4,2)
+        self.data = np.asarray(cv2.imread('img.jpg'),dtype = "int32")
+        # self.data = np.random.rand(4,2,3)
     
     def printData(self):
-        print(self.data)
+        print(self.data.shape)
 
     def connectToClients(self):
         while(len(self.clients) < self.no_of_clients):
             self.clients.append(self.s.accept())
+        self.s.close()
 
     def sendMessageToAll(self):
         for i in range(len(self.clients)):
@@ -61,6 +71,8 @@ class Host():
     def sendData(self):
         t = int(self.data.shape[0]/self.no_of_clients)
         for i in range(len(self.clients)):
-            self.clients[i][0].send((self.data[i*t:(i + 1)*t]).tostring())
+            sendingMessage = (self.data[i*t:(i + 1)*t]).tostring()
+            print("sending %d",len(sendingMessage))
+            self.clients[i][0].send(sendingMessage)
 
     
